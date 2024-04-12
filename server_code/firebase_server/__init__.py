1c5ca41ae18f194e994e77026b77e66ea57520c5
Luis Anthony Saenz <lsaenz9104@gmail.com>
2024-04-12 10:19:31 -0700

    adding ability to change backend database in server

--- server_code/firebase_server/__init__.py
+++ server_code/firebase_server/__init__.py
@@ -1,19 +1,19 @@
 import anvil.server
 
 
-def init_firebase_server(skd_config,bucket_id=None):
+def init_firebase_server(skd_config,bucket_id=None,name='default'):
   '''Intializes the serer side firestore sdk'''
   import firebase_admin
   from firebase_admin import credentials, firestore, storage
 
   if bucket_id is None:
-    firebase_admin.initialize_app(credentials.Certificate(skd_config))
+    firebase_admin.initialize_app(credentials.Certificate(skd_config),name=name)
     return firestore.client()
   else:
-    firebase_admin.initialize_app(credentials.Certificate(skd_config),{'storageBucket': bucket_id})
+    firebase_admin.initialize_app(credentials.Certificate(skd_config),{'storageBucket': bucket_id},name=name)
     return firestore.client(), storage.bucket()
 
-    
+
 """
 Client callable functions:
 """
@@ -22,18 +22,16 @@ Client callable functions:
 def _fs_get_anvil_firestore_auth_token(user_claims=[]):
   import firebase_admin
   from firebase_admin import auth
-  
+
   user = anvil.users.get_user()
   user_uid = str(user.get_id())
-  
+
   #create additional claims -> add specific user relevant claims to support security rules
   additional_claims = {
     'user_uid':user_uid,
   }
   for claim in user_claims:
     additional_claims[claim] = user[claim]
-  
-  #Create Firebase Token and return string
-  return auth.create_custom_token(user_uid, additional_claims).decode("utf-8") 
 
-  
+  #Create Firebase Token and return string
+  return auth.create_custom_token(user_uid, additional_claims).decode("utf-8")

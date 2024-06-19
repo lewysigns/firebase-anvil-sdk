a8f30768f462a3ca79ba537df91c4c4ac364ecb6
Luis Anthony Saenz <lsaenz9104@gmail.com>
2024-06-19 10:33:43 -0700

    debugging firestore client

--- server_code/firebase_server/__init__.py
+++ server_code/firebase_server/__init__.py
@@ -8,6 +8,8 @@ def init_firebase_server(skd_config,bucket_id=None,name='default'):
 
   if bucket_id is None:
     app = firebase_admin.initialize_app(credentials.Certificate(skd_config),name=name)
+    firestore_client = firestore.client(app=app)
+    firestore_client._database_string_internal = (f"projects/letem-prod/databases/{name}")
     return firestore.client(app=app)
   else:
     app = firebase_admin.initialize_app(credentials.Certificate(skd_config),{'storageBucket': bucket_id},name=name)
